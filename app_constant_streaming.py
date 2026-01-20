34b7b2d09545400000de88da0bf7ba3633306ff8
wy85-bit <149866791+wy85-bit@users.noreply.github.com>
2026-01-20 19:02:59 +0100

    Update app_constant_streaming.py

--- app_constant_streaming.py
+++ app_constant_streaming.py
@@ -2,9 +2,20 @@ import streamlit as st
 from google.cloud import speech
 import os
 
+import streamlit as st
+from google.cloud import speech
+from google.oauth2 import service_account
 
+# This pulls the data from the 'Secrets' section you filled out in Streamlit
+try:
+    creds_info = st.secrets["gcp_service_account"]
+    credentials = service_account.Credentials.from_service_account_info(creds_info)
+    client = speech.SpeechClient(credentials=credentials)
+except Exception as e:
+    st.error(f"Credentials not found or invalid: {e}")
+    st.stop()
+    
 st.set_page_config(page_title="VoiceNotes", layout="centered")
-
 # Replace with your key path
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gen-lang-client-0785559721-35a88d77a6dd.json"
 
@@ -44,4 +55,5 @@ if audio_value:
         # We pass the bytes directly
         result_text = transcribe_audio_file(audio_value.getvalue())
         st.subheader("Final Transcription:")
+
         st.write(result_text)
